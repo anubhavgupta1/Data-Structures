c3894545031e2f40b75716ee0b8ff0421cbedddf
anubhavgupta1 <41202057+anubhavgupta1@users.noreply.github.com>
2018-11-10 12:12:53 +0530

    Create choice.h

---
+++ C_Programs/1. Linked List/DLL/choice.h
@@ -0,0 +1,37 @@
+void choice()
+{	
+	int n;
+	list head = NULL;
+	printf("\nChoice 1 = createBeg()\nChoice 2 = createEnd()");
+	printf("\nChoice 3 = insertRandom()\nChoice 4 = deleteNode()");
+	printf("\nChoice 5 = printList()");
+	while(1)
+	{
+		printf("\nEnter the choice : ");scanf("%d",&n);
+		switch(n)
+		{
+			case 1:
+			head = createBeg(head);
+			break;
+		
+			case 2 :
+			createEnd(&head);
+			break;
+
+			case 3 :
+			insertRandom(&head);
+			break;
+
+			case 4 :
+			deleteNode(&head);
+			break;
+			
+			case 5 :
+			printList(head);
+			break;
+
+			default:
+			exit(0);
+		}
+	}	
+}
